e74353bb4ada64f65116284cf1a154e757507a34
marcgregi <marc@hypedigital.co>
2019-01-14 21:47:03 +0000

    hardcode function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
